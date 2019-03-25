d1d9d174d8004a779c08a4cb55f359d662318174
EnixCoda <enixcoda@gmail.com>
2019-03-25 12:00:51 +0800

    test: launch with Chrome

--- tests/launch.js
+++ tests/launch.js
@@ -3,11 +3,14 @@ const path = require('path')
 
 const CRX_PATH = path.resolve(__dirname, '../dist')
 
-exports.launch = (options) =>
+const pathToChrome = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+
+exports.launch = options =>
   puppeteer.launch({
     headless: false, // required for extensions
     args: [`--disable-extensions-except=${CRX_PATH}`, `--load-extension=${CRX_PATH}`],
-    ...options
+    executablePath: pathToChrome,
+    ...options,
   })
 
 exports.getGitako = () => window.GITAKO_DEBUG_INTERFACE
